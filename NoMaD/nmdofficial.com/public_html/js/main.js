056be8f8c527cb4b986eb8bd2d8f18833b401a0b
Charles K <kncharlot@gmail.com>
2016-04-08 21:16:36 -0400

    Draft

--- NoMaD/nmdofficial.com/public_html/js/main.js
+++ NoMaD/nmdofficial.com/public_html/js/main.js
@@ -1,1 +1,54 @@
-
+(function (d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0];
+                                    if (d.getElementById(id))
+                                        return;
+                                    js = d.createElement(s);
+                                    js.id = id;
+                                    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                }(document, 'script', 'facebook-jssdk'));
+                                
+!function (d, s, id) {
+                                    var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                                    if (!d.getElementById(id)) {
+                                        js = d.createElement(s);
+                                        js.id = id;
+                                        js.src = p + "://platform.twitter.com/widgets.js";
+                                        fjs.parentNode.insertBefore(js, fjs);
+                                    }
+                                }(document, "script", "twitter-wjs");
+                                
+(function (d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0];
+                                if (d.getElementById(id))
+                                    return;
+                                js = d.createElement(s);
+                                js.id = id;
+                                js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+                                fjs.parentNode.insertBefore(js, fjs);
+                            }(document, 'script', 'facebook-jssdk'));
+                            
+!function (d, s, id) {
+                                var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
+                                if (!d.getElementById(id)) {
+                                    js = d.createElement(s);
+                                    js.id = id;
+                                    js.src = p + '://platform.twitter.com/widgets.js';
+                                    fjs.parentNode.insertBefore(js, fjs);
+                                }
+                            }(document, 'script', 'twitter-wjs');
+                            
+(function (b, o, i, l, e, r) {
+                                b.GoogleAnalyticsObject = l;
+                                b[l] || (b[l] =
+                                        function () {
+                                            (b[l].q = b[l].q || []).push(arguments)
+                                        });
+                                b[l].l = +new Date;
+                                e = o.createElement(i);
+                                r = o.getElementsByTagName(i)[0];
+                                e.src = '//www.google-analytics.com/analytics.js';
+                                r.parentNode.insertBefore(e, r)
+                            }(window, document, 'script', 'ga'));
+                            ga('create', 'UA-XXXXX-X', 'auto');
+                            ga('send', 'pageview');                            
